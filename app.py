57e969f11ddf6cbcf85b76de5aa3c3d646e0935d
Dev <133972286+GreatCoder1000@users.noreply.github.com>
2024-12-23 12:35:32 +0000

    Use Gunicorn

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
 @app.route("/greet")
 def greet():
     name = request.args.get("name", "world")
     return render_template("greet.html", name=name)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # This line can be removed when using Gunicorn
